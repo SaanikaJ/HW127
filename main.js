6253193a49f674e724f7493a060747165b43d2e3
SaanikaJ <74892925+SaanikaJ@users.noreply.github.com>
2021-08-11 08:14:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+    song = loadSound("music.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(450, 450); 
+    canvas.center(); 
+
+    video = createCapture(VIDEO); 
+    video.hide(); 
+}
+
+function draw(){
+    image(video, 0, 0, 450, 450); 
+}
